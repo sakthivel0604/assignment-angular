e9e2e4dc3b7953ff800afd71fa4ca87f246dbe62
sakthivel0604 <it214040@gmail.com>
2021-01-22 16:53:48 +0530

    First commit

---
+++ misnum/src/app/value/value.component.ts
@@ -0,0 +1,17 @@
+import { NgIf } from '@angular/common';
+import { Component, OnInit} from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.css']
+})
+export class ValueComponent  {
+
+ Value = new FormControl('');
+ if(value%2===0){
+   
+ }
+  }
+}
